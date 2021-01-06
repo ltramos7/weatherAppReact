7a4fce393c48db854c77b1decd61ac06c2a4c8bc
Linda Ramos <ltramos7@gmail.com>
2021-01-06 10:25:04 -0600

    feat: added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome To WeatherApp!</h1>
+            </div>
+        )
+    }
+}
+
