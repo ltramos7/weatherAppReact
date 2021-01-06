7fa28d4a85ea1b58498245f697c4292316cefbde
Linda Ramos <ltramos7@gmail.com>
2021-01-06 10:33:29 -0600

    feat: created & added WeatherCard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Button from './components/Button'
+import WeatherCard from './components/WeatherCard'
 
 function App() {
   return (
     <div>
       <Header/>
       <Button/>
+      <WeatherCard/>
     </div>
   );
 }
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WeatherCard extends Component {
+    render() {
+        return (
+            <div>
+                Your current weather info
+            </div>
+        )
+    }
+}
