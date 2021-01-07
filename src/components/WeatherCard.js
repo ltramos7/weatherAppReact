c827d04cc9a0ff4e4b3cdc7641598112247a57b3
Linda Ramos <ltramos7@gmail.com>
2021-01-07 16:13:52 -0600

    feat: able to show local time on front end

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react'
 
 export default class WeatherCard extends Component {
-
-   
-    
-    getLocalTime = (time) => {
-        // let today = new Date(this.props.localWeather.dt*1000)
-        let today = new Date(time*1000)
-        let [month, day, year] = today.toLocaleDateString("en-US").split("/")
-        let hour = today.getHours()
-        let minute = today.getMinutes()
-        console.log(month, day, year, hour, minute)
-        
-    }
-    componentDidUpdate = () => {
-        this.getLocalTime(this.props.localWeather.dt)
-    }
+    //this.props.localWeatherInfo
+    //this.props.localTime
     
     render() {
-        let localWeather = this.props.localWeather
-        
+        console.log(this.props.localTime)
         return (
             <div id="local-weather-info-container">
-                <h2 id="city-location">{localWeather.name}</h2>
-                <p id="current-time">Time goes here</p>
-                {/* <p id="current-time">as of {hour} - {month}/{day}/{year}</p> */}
+                <h2 id="city-location">{this.props.localWeatherInfo.name}</h2>
+                <p id="current-time">as of {this.props.localTime[3]}:{this.props.localTime[4]} - {this.props.localTime[0]}/{this.props.localTime[1]}/{this.props.localTime[2]}</p>
                 <p id="current-temp"></p>
                 <p id="weather-description"></p>
                 <p id="local-weather-info"></p>
