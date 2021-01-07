73c5944f009e79b33f768a0db257b87f4df2b336
Linda Ramos <ltramos7@gmail.com>
2021-01-07 10:38:04 -0600

    refactor: updated this.props.localWeather to time for getLocaTime func

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 
 export default class WeatherCard extends Component {
+
+   
     
-    getLocalTime = () => {
-        let today = new Date(this.props.localWeather.dt*1000)
-        // let today = new Date(Number(data.dt)*1000)
-        console.log(today)
+    getLocalTime = (time) => {
+        // let today = new Date(this.props.localWeather.dt*1000)
+        let today = new Date(time*1000)
         let [month, day, year] = today.toLocaleDateString("en-US").split("/")
         let hour = today.getHours()
         let minute = today.getMinutes()
@@ -13,19 +14,16 @@ export default class WeatherCard extends Component {
         
     }
     componentDidUpdate = () => {
-        this.getLocalTime()
+        this.getLocalTime(this.props.localWeather.dt)
     }
     
     render() {
-        // let today = new Date(Number(this.props.localWeather.dt)*1000)
-        // let [month, day, year, hour] = today.toLocaleDateString("en-US").split("/")
         let localWeather = this.props.localWeather
-        console.log(this.props.localWeather)
         
         return (
             <div id="local-weather-info-container">
                 <h2 id="city-location">{localWeather.name}</h2>
-                <p id="current-time" onChange={this.getLocalTime}>Time goes here</p>
+                <p id="current-time">Time goes here</p>
                 {/* <p id="current-time">as of {hour} - {month}/{day}/{year}</p> */}
                 <p id="current-temp"></p>
                 <p id="weather-description"></p>
