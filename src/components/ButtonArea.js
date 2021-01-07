6ff9a8f844eb41b09d41c8b7d87255c971c288e3
Linda Ramos <ltramos7@gmail.com>
2021-01-07 16:14:31 -0600

    style: erased comments and console logs

--- src/components/ButtonArea.js
+++ src/components/ButtonArea.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
 export default class ButtonArea extends Component {
+  
     render() {
         return (
             <div>
-                <button onClick={this.props.getLocalWeather}>Current Weather</button>
+                {/* <button onClick={() => {this.props.getLocalWeather(); this.props.getLocalTime();}}>Current Weather</button> */}
+                <button onClick={this.props.getCurrentWeather}>Current Weather</button>
             </div>
         )
     }
