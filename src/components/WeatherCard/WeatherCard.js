ac020090dc7b2cc83dd11d15f9edd360b21a2973
Linda Ramos <ltramos7@gmail.com>
2021-01-07 22:30:09 -0600

    refactor: rounded temp values

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -8,9 +8,9 @@ export default class WeatherCard extends Component {
             <div className={style.localWeatherInfoContainer}>
                 <h2 id="city-location">{this.props.localWeatherInfo.name} Weather</h2>
                 <p id="current-time">as of {this.props.localTime[0]}/{this.props.localTime[1]}/{this.props.localTime[2]} at {this.props.localTime[3]}:{this.props.localTime[4]}</p>
-                <p id="current-temp">Currently {this.props.localWeatherInfo.main.temp}° F and feels like {this.props.localWeatherInfo.main.feels_like}° F</p>
+                <p id="current-temp">Currently {Math.round(this.props.localWeatherInfo.main.temp)}° F and feels like {Math.round(this.props.localWeatherInfo.main.feels_like)}° F</p>
                 <p id="weather-description">{this.props.localWeatherInfo.weather[0].description}</p>
-                <p id="local-weather-info">Hi: {this.props.localWeatherInfo.main.temp_max}° / Low: {this.props.localWeatherInfo.main.temp_min}°</p>
+                <p id="local-weather-info">Hi: {Math.round(this.props.localWeatherInfo.main.temp_max)}° F / Low: {Math.round(this.props.localWeatherInfo.main.temp_min)}° F</p>
             </div>
         )
     }
