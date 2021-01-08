7902c76340d7082856317360885c9426b1ab3a2f
Linda Ramos <ltramos7@gmail.com>
2021-01-07 22:09:16 -0600

    refactor: applied css to ButtonArea component

--- src/components/ButtonArea/ButtonArea.js
+++ src/components/ButtonArea/ButtonArea.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import style from './ButtonArea.module.css'
 
 export default class ButtonArea extends Component {
   
     render() {
         return (
-            <div className="current-weather-btn">
+            <div className={style.currentWeatherBtn}>
                 {/* <button onClick={() => {this.props.getLocalWeather(); this.props.getLocalTime();}}>Current Weather</button> */}
                 {/* <button onClick={()=>{this.props.getCurrentWeather();this.props.toggleLoadingStatus();}}>Current Weather</button> */}
                 <button onClick={this.props.getCurrentWeather}>Current Weather</button>
