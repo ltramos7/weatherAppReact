4b28f402e3996ef8efb04aa685ab7cb7370c07a2
Linda Ramos <ltramos7@gmail.com>
2021-01-07 22:09:54 -0600

    refactor: applied css to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 
 import React, { Component } from 'react'
-import './Header.js'
+import style from './Header.module.css'
 
 
 export default class Header extends Component {
     render() {
         return (
             <div>
-                <h1 className="welcome-title">Welcome To WeatherApp!</h1>
+                <h1 className={style.welcomeTitle}>Welcome To WeatherApp!</h1>
+                
             </div>
         )
     }
