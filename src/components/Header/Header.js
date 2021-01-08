05a55d9798ba1af69a868957b93880eebbd2dcb2
Linda Ramos <ltramos7@gmail.com>
2021-01-07 20:36:04 -0600

    refactor: created header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 
 import React, { Component } from 'react'
+import './Header.js'
+
 
 export default class Header extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome To WeatherApp!</h1>
+                <h1 className="welcome-title">Welcome To WeatherApp!</h1>
             </div>
         )
     }
